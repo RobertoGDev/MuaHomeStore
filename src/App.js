dd35eadaef7440446b0ead3cbfa3300ef3e70da3
Roberto <rob.vicalvaro@gmail.com>
2020-01-26 01:19:58 +0100

    ejemplo de hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const update = function() {
+    setcount(count + 1)
+  };
+
+  const [count, setcount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Has hecho click {count} veces</p>
+      <button onClick={() => update()}>Pulsa hijo de puta</button>
     </div>
   );
 }
